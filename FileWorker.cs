6b14e354d8996e5611e3e993c409e93c02265797
Anton Zaitsev <zay-toxa@mail.ru>
2022-04-09 22:16:37 +0300

    Add project files.

---
+++ FileWorker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+
+namespace HomeWorkOfLesson27
+{
+    class FileWorker
+    {
+        public static void CreateDir(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            try
+            {
+                if (di.Exists)
+                {
+                    Console.WriteLine("Такой каталог уже существует.");
+                    return;
+                }
+                di.Create();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла непредвиденная ошибка");
+            }
+        }
+
+        public static void CreateFile(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("Такой файл уже существует.");
+                    return;
+                }
+                File.Create(path).Close();
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Отсутствуют права на запись");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла непредвиденная ошибка");
+            }
+        }
+
+        public static void WriteToFile(string path, string message)
+        {
+            try
+            {
+                using StreamWriter writer = new StreamWriter(path);
+                {
+
+                    writer.AutoFlush = true;
+                    writer.Write(message);
+                    return;
+                }             
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Отсутствуют права на запись");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла непредвиденная ошибка");
+            }
+        }
+
+        public static void GetTextFromFile(string path)
+        {
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Такого файла не существует.");
+                return;
+            }
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                    Console.WriteLine($"{Path.GetFullPath(path)}: {sr.ReadToEnd()}");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Отсутствуют права на запись");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла непредвиденная ошибка");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace HomeWorkOfLesson27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        private void Start()
+        {
+            var fw = new FileWorker();
+
+            string pathDir1 = @"c:\Otus\TestDir1";
+            string pathDir2 = @"c:\Otus\TestDir2";
+            FileWorker.CreateDir(pathDir1);
+            FileWorker.CreateDir(pathDir2);
+            MainLogigMethod(fw, pathDir1);
+            MainLogigMethod(fw, pathDir2);
+        }
+
+        private void MainLogigMethod(FileWorker fw, string path)
+        {                     
+            for (int i = 0; i < 10; i++)
+            {
+                string fileName = @$"{path}\File{i + 1}.txt";
+
+                FileWorker.CreateFile(fileName);
+
+                byte[] bytes = Encoding.Default.GetBytes(Path.GetFileName(fileName));
+                string fileNameUTF8 = Encoding.UTF8.GetString(bytes);
+
+                DateTime dateTime = DateTime.Now;
+                CultureInfo culture = new CultureInfo("ru-ru");
+                string data = $"{fileNameUTF8} {dateTime.ToString("d MMMM yyyy HH:mm:ss.fff", culture)}";
+
+                FileWorker.WriteToFile(fileName, data);
+                FileWorker.GetTextFromFile(fileName);
+            }
+        }
+    }
+}
